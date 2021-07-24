046d14da30ec10714ee30e7bcda9a0ec6f5e0942
king <king>
2021-07-24 10:32:33 +0800

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Admin from '../components/admin.vue'
+import Admin from '../components/admin/admin'
 import Welcome from '../components/welcome.vue'
-import MerchadiseMenu from "../components/merchandiseMenu"
-import AddMerchandise from "../components/addMerchandise"
-import Login from "../components/login.vue"
-import BrandAdministration from "../components/brandAdministration.vue"
-import AddBrand from "../components/addBrand.vue"
+import MerchadiseMenu from "../components/product/product"
+import AddMerchandise from "../components/product/addProduct"
+import Login from "../components/admin/adminLogin"
+import BrandAdministration from "../components/brand/productBrand"
+import AddBrand from "../components/brand/addBrand"
 import ClassifyMerchandise from "../components/classifyMerchandise.vue"
-import AddCategory from "../components/addCategory.vue"
-import ChangeProduct from "../components/changeProduct.vue"
-import ProductCategory from "../components/productCategory.vue"
-import Register from "../components/register.vue"
-import UserLogin from "../components/userLogin.vue"
-import UserRegister from "../components/userRegister.vue"
+import AddCategory from "../components/category/addCategory"
+import ChangeProduct from "../components/product/changeProduct"
+import ProductCategory from "../components/category/productCategory"
+import Register from "../components/admin/adminRegister"
+import UserLogin from "../components/user/userLogin"
+import UserRegister from "../components/user/userRegister"
 
 
 Vue.use(VueRouter)
